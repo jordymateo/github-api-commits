6796645907ffdf5092a112d8aa3ebd4833cadf68
Jordy Mateo <jordymateo@hotmail.es>
2023-01-05 14:11:29 -0400

    Updated get commits end-point to include pagination

--- server/src/github/github.controller.ts
+++ server/src/github/github.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { GithubService } from './github.service';
 
 @Controller("github")
@@ -6,9 +6,9 @@ export class GithubController {
   constructor(private readonly githubService: GithubService) { }
 
   @Get('commits')
-  async getCommits(@Res() res) {
+  async getCommits(@Query('per_page') per_page: number, @Query('page') page: number, @Res() res) {
     try {
-      const data = await this.githubService.getCommits();
+      const data = await this.githubService.getCommits(per_page, page);
       return res.status(HttpStatus.OK).json(data);
     } catch (ex) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json("Unexpected error. " + ex);
--- server/src/github/github.service.ts
+++ server/src/github/github.service.ts
@@ -16,10 +16,12 @@ export class GithubService {
     return data;
   }
 
-  async getCommits(): Promise<Commit[]> {
-    const { data } = await this.octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
+  async getCommits(per_page: number, page: number): Promise<Commit[]> {
+    const {data} = await this.octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
       owner: process.env.PROJECT_OWNER,
-      repo: process.env.PROJECT_REPO
+      repo: process.env.PROJECT_REPO,
+      per_page,
+      page
     });
     return data;
   }
